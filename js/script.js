98ce6f5f54191eb4baebd65b4533249266bcd473
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-30 16:15:11 +0100

    programma js completo

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// creare una to-do list, come quella fatta insieme stamattina, utilizzando Handlebars come templating engine.
+
+$(document).ready (
+    function () {
+        var confirmButton = $("#confirm_btn");
+
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+
+
+        $(document).on( "click", ".delete",
+            function () {
+                $(this).parent().remove();
+            }
+        );
+
+        confirmButton.click(
+            function () {
+                var userInput = $("#user-input").val();
+
+                if (userInput == "") {
+                    alert("Campo vuoto");
+                } else {
+                    var context = {
+                        'text': userInput,
+                    }
+                    var html = template(context);
+                    $("#to-do").append(html);
+                }
+
+            }
+        );
+
+        $("#user-input").keyup(function(event){
+            if (event.which == 13 || event.keyCode == 13) {
+                confirmButton.trigger("click");
+            }
+        });
+
+
+    }
+);
